a455f8ea1a9756ca49e1966019ecc997dd5321fd
erreib <erreib@gmail.com>
2023-09-27 17:25:36 -0400

    adding sign on after successful account creation

--- src/Signup.js
+++ src/Signup.js
@@ -1,35 +1,51 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useUser } from './UserContext';
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
-function Signup() {
+const Signup = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [error, setError] = useState(null);
 
+  const { setUser, setToken } = useUser();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
       const response = await axios.post(`${backendUrl}/api/auth/signup`, {
         username,
         password,
       });
 
-      if (response.status === 201) {
-        // setMessage('User successfully created');
-        navigate(`/${username}`);
-      }
+      // Assume the server sends a token upon successful signup
+      const { userId, token } = response.data;
+
+      // Update user state
+      setUser({
+        id: userId,
+        username: username,
+      });
+
+      // Update token state
+      setToken(token);
+
+      // Store in localStorage
+      localStorage.setItem('userId', userId);
+      localStorage.setItem('token', token);
+      localStorage.setItem('username', username);
+
+      // Clear any errors
+      setError(null);
+
+      // Navigate to the user's dashboard (replace 'username' with the actual username)
+      navigate(`/${username}`);
     } catch (error) {
-      if (error.response) {
-        setMessage(`Error: ${error.response.data.message}`);
-      } else {
-        setMessage('Something went wrong');
-      }
+      console.error('Signup error:', error);
+      setError('Could not sign up, please try again');
     }
   };
 
@@ -37,29 +53,24 @@ function Signup() {
     <div>
       <h1>Signup</h1>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+        <input 
+          type="text" 
+          placeholder="Username" 
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input 
+          type="password" 
+          placeholder="Password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
         <button type="submit">Signup</button>
       </form>
-      {message && <div>{message}</div>}
+      {error && <p>{error}</p>}
     </div>
   );
-}
+};
 
 export default Signup;
